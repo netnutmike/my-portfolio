c8e5a280051e0609b979f4dd4cafe96976f32f6a
Mike Myers <netnutmike@gmail.com>
2017-12-21 23:17:49 -0500

    update to lamba code

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -10,14 +10,31 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:568836648543:deployPortfolioTopic')
     
+    # set default locations while creating the dictionary
+    
+    location = {
+        "bucketName": 'porfoliobuild.miketest',
+        "objectKey" : 'portfoliobuild.zip'
+    }
+    
     try:
+        job = event.get("CodePipeline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+         
+        #Save in the logs where we built from, just in case         
+        print "Building portfolio from " + str(location)  
+        
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
     
         portfolio_bucket = s3.Bucket('portfolio.mikestest.online')
-        build_bucket = s3.Bucket('porfoliobuild.miketest')
+        build_bucket = s3.Bucket(location["bucketName"])
         
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
         
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -28,6 +45,9 @@ def lambda_handler(event, context):
         
         print "New Portfolio Deployed, sending SNS"
         topic.publish(Subject="New Portfolio Code Deployed", Message="New portfolio code was deployed to the production S3 bucket")
+        if job:
+            codepipeline = boto3.client("codepipeline")
+            codepipeline.put_job_success_result(jobId=job["id"])
         
     except:
         topic.publish(Subject="portfolio deploy failed", Message="New portfolio code failed to deploy to the production S3 bucket")
